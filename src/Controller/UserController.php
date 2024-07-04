3f75398f59439f62a2fd668251fbfc5da9ae602f
SwaiiZ <le.bail.bastienlb@gmail.com>
2024-07-04 16:11:42 +0200

    [ADD] Ranking route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\HomeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,16 +12,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
 
-    public function __construct(private readonly Security $security)
+    public function __construct(private readonly Security $security, private HomeService $homeService)
     {
     }
 
-    #[Route('/api/myprofile', name: 'app_user')]
+    #[Route('/api/myprofile', name: 'my_profile')]
     public function index(): Response
     {
         return new JsonResponse([
             'id' => $this->security->getToken()->getUser()->getId(),
             'teamId' => $this->security->getToken()->getUser()->getTeam()->getId()
         ], 200, []);
+    }
+
+    #[Route('/api/rankings', name: 'rankings')]
+    public function rankings(): Response
+    {
+        return new JsonResponse([
+            'rankings' => $this->homeService->getRankings(),
+        ]);
     }
 }
